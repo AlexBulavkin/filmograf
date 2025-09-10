a16c2277046a931f33fe03fdf001117ec9b704a4
alexbulavkin <alexbulavkin@gmail.com>
2025-09-11 02:52:12 +0700

    menu fix

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,51 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 import { Flex } from "@chakra-ui/react";
 
 export default function Menu(){
     return (
-        <Flex color={"black"} fontSize="16px" fontWeight={"medium"} gap="15px" my="30px">
-            <Link to="/">Все фильмы</Link>
-            <Link to="/saved">Избранное</Link>
-            <Link to="/add">Добавить фильм</Link>
+        <Flex gap="15px" my="30px" alignItems="center">
+            <NavLink 
+                to="/"
+                style={({ isActive }) => ({
+                    fontSize: "16px",
+                    fontWeight: "medium",
+                    textDecoration: "none",
+                    color: isActive ? "#3B82F6" : "#374151",
+                    hover: {
+                        textDecoration: "underline"
+                    }
+                })}
+            >
+                Все фильмы
+            </NavLink>
+            <NavLink 
+                to="/saved"
+                style={({ isActive }) => ({
+                    fontSize: "16px",
+                    fontWeight: "medium",
+                    textDecoration: "none",
+                    color: isActive ? "#3B82F6" : "#374151",
+                    hover: {
+                        textDecoration: "underline"
+                    }
+                })}
+            >
+                Избранное
+            </NavLink>
+            <NavLink 
+                to="/add"
+                style={({ isActive }) => ({
+                    fontSize: "16px",
+                    fontWeight: "medium",
+                    textDecoration: "none",
+                    color: isActive ? "#3B82F6" : "#374151",
+                    hover: {
+                        textDecoration: "underline"
+                    }
+                })}
+            >
+                Добавить фильм
+            </NavLink>
         </Flex>
     )
 }
-
